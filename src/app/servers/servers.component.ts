b28af3f27c4879ed86b71252bfc8e8cd17b7ef14
Juan David Gaitan <jdgaitanf@unal.edu.co>
2023-12-13 21:30:27 -0500

    project course

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  /* template: '<app-server></app-server><app-server></app-server>', */
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCrationStatus: String = 'No server was created!';
+  serverName: String = "Testserver";
+
+  constructor() {
+    /* Constructor is just a method executed by the point of time this component is created by Angular */
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2500);
+  }
+
+  ngOnInit() {}
+  onCreateServer() {
+    /* When namig DeclareFunctionStmt, the On prefix means that this is triggered by something from the template */
+    this.serverCrationStatus =  "The server was created! Name is " + this.serverName
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}

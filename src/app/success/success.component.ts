ea6f61388c1207315ad4f746d40e0ca766a09cec
Juan David Gaitan <jdgaitanf@unal.edu.co>
2023-12-13 21:41:39 -0500

    initial commit

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: '[app-sucess]',
+  templateUrl: './success.component.html',
+  styleUrls: ['./success.component.css']
+})
+export class SuccessComponent {}

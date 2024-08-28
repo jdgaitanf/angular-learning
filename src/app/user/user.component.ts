48f815a9654279f1c51569cbe787a557d9654273
David G <jdgaitanf@unal.edu.co>
2024-08-27 21:49:43 -0500

    Tasks component created. Shows name of user dinamically

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  output
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +13,21 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  // select = output<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath =  computed(()=>{return 'assets/users/' + this.avatar();})
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectedUser() {}
+  onSelectedUser() {
+    this.select.emit(this.id);
+  }
 }

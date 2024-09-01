e61cf775668fc9292a1e5993c8984bda2cdcdefd
David G <jdgaitanf@unal.edu.co>
2024-08-31 19:20:32 -0500

    User tasks are rendered

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  output
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { type User } from './user.model';
+// type User = {
+//     id: string;
+//     avatar: string;
+//     name: string;
+//   }
+
 
 @Component({
   selector: 'app-user',
@@ -13,9 +14,11 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter<string>();
 
   // select = output<string>();
@@ -24,10 +27,10 @@ export class UserComponent {
   // imagePath =  computed(()=>{return 'assets/users/' + this.avatar();})
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }

e89f728028df1c5a74da98a218bff6d063c2811c
David G <jdgaitanf@unal.edu.co>
2024-09-01 15:47:28 -0500

    User tasks can now be deleted

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,6 +15,7 @@ import { type User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
 
   // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;

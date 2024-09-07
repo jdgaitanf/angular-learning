a85efae2a72193e439c223e91b17c484646a027f
David G <jdgaitanf@unal.edu.co>
2024-09-07 18:03:52 -0500

    Added dependency injection

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,32 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onSubmit() {
+
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
+
   onCancel() {
-    this.cancel.emit();
+    this.cancel.emit()
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 // type User = {
 //     id: string;
 //     avatar: string;
@@ -12,6 +13,7 @@ import { type User } from './user.model';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
